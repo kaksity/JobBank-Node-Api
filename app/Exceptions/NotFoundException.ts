b6f1bd1aa938dcd1ce4552322efdf2c8f5f1bd0d
Dauda Pona <daudapona2@gmail.com>
2023-01-08 17:37:36 +0100

    feat: added exception for error handling

---
+++ app/Exceptions/NotFoundException.ts
@@ -0,0 +1,27 @@
+import { Exception } from '@adonisjs/core/build/standalone'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+/*
+|--------------------------------------------------------------------------
+| Exception
+|--------------------------------------------------------------------------
+|
+| The Exception class imported from `@adonisjs/core` allows defining
+| a status code and error code for every exception.
+|
+| @example
+| new NotFoundException('message', 500, 'E_RUNTIME_EXCEPTION')
+|
+*/
+export default class NotFoundException extends Exception {
+  constructor(message: string) {
+    super(message, 404)
+  }
+  public async handle(error: this, { response }: HttpContextContract) {
+    return response.notFound({
+      success: false,
+      type: 'https://tools.ietf.org/html/rfc7231#section-6.5.4',
+      title: 'The specified resource was not found.',
+      detail: error.message,
+    })
+  }
+}
