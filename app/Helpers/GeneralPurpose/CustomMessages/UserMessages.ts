93d9fddd538ba6a042a842009bd7998db96e0ce9
Dauda Pona <daudapona2@gmail.com>
2023-01-09 07:47:13 +0100

    feat: added user login

--- app/Helpers/GeneralPurpose/CustomMessages/UserMessages.ts
+++ app/Helpers/GeneralPurpose/CustomMessages/UserMessages.ts
@@ -1,3 +1,4 @@
 export const USER_DOES_NOT_EXIST = 'User Account does not exist'
 export const USER_ALREADY_EXIST = 'User Account already exist'
 export const USER_ACCOUNT_CREATED = 'User Account was created successfully'
+export const USER_ACCOUNT_NOT_ACTIVE = 'User Account is not active'
