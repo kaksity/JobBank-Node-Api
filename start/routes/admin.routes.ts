be277c667718cf953f74557d9442ebce632ee3ba
Dauda Pona <daudapona2@gmail.com>
2023-01-16 16:41:54 +0100

    feat: added authentication middleware to the admin

--- start/routes/admin.routes.ts
+++ start/routes/admin.routes.ts
@@ -1,19 +1,21 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(function () {
-  Route.group(function () {
-    Route.post('/register', 'AuthController.register')
-    Route.post('/login', 'AuthController.login')
-  })
-    .prefix('/auth')
-    .namespace('App/Controllers/Http/V1/Admin/Auth')
-  Route.resource('/users', 'UsersController')
+  Route.post('/register', 'AuthController.register')
+  Route.post('/login', 'AuthController.login')
+})
+  .prefix('/api/v1/admin/auth')
+  .namespace('App/Controllers/Http/V1/Admin/Auth')
 
+
+Route.group(function () {
+  Route.resource('/users', 'UsersController')
   Route.resource('/users/:userId/profiles', 'UserProfilesController')
   Route.resource('/users/:userId/work-experiences', 'UserWorkExperiencesController')
   Route.resource('/users/:userId/qualifications', 'UserQualificationDocumentsController')
   Route.resource('/users/:userId/skill-sets', 'UserSkillSetsController')
   Route.resource('/users/:userId/educations', 'UserEducationsController')
 })
+  .middleware(['auth'])
   .prefix('/api/v1/admin')
   .namespace('App/Controllers/Http/V1/Admin')
